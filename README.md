0051b0d6fbd63c61f3ac1b7adcdda1c0443b7eca
akasmall <aka_sm@ro.ru>
2023-09-26 12:43:46 +0300

    added game brain_gcd and link to asciinema.org in README.md

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 <a href="https://codeclimate.com/github/akasmall/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/a83c753bdcdabd8080cb/maintainability" /></a>
 <a href="https://codeclimate.com/github/akasmall/python-project-49/test_coverage"><img src="https://api.codeclimate.com/v1/badges/a83c753bdcdabd8080cb/test_coverage" /></a>
 https://asciinema.org/a/y20Hg574NJg23MmF5vaZFB2mG
+https://asciinema.org/a/Aihew44WUsIfo7J8emRt35ajz
--- brain_games/question_answer.py
+++ brain_games/question_answer.py
@@ -1,40 +1,45 @@
 import prompt
 import random
+import math
 
 
-def greeting_user():
+def greeting_user():                            # приветствуем юзера
     print('Welcome to the Brain Games!')
 
 
-def give_name():
+def give_name():                                # узнаем имя
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
 
 
-def task_to_the_user(name_game):
+def task_to_the_user(name_game):                # объясняем задание юзеру
     if name_game == 'even':
         print('Answer "yes" if the number is even, otherwise answer "no".')
     elif name_game == 'calc':
         print('What is the result of the expression?')
+    elif name_game == 'gcd':
+        print('Find the greatest common divisor of given numbers.')
     else:
         print('Something went wrong. Unknown game!')
 
 
-def random_sign(sign):
+def random_sign(sign):                  # получаем случайный знак из списка
     random.shuffle(sign)
     return random.choice(sign)
 
 
-def random_number(number_1, number_2):
+def random_number(number_1, number_2):          # получаем случайное число
     return random.randint(number_1, number_2)
 
 
+# задаем вопрос/задачу юзеру
 def question(random_number_1, random_number_2='', random_sign=''):
     result = f'{random_number_1} {random_sign} {random_number_2}'
     print(f'Question: {result}')
 
 
+# проверяем ответ для игры brain_even
 def checking_answer_even(value_task, answer_user):
     if answer_user.lower() != 'yes':
         if answer_user.lower() != 'no':
@@ -46,13 +51,15 @@ def checking_answer_even(value_task, answer_user):
         return False
 
 
-def checking_answer_calc(value_task, answer_user):
+# проверяем ответ для игры brain_calc и brain_gcd
+def checking_answer(value_task, answer_user):
     if value_task == answer_user:
         return True
     else:
         return False
 
 
+# получаем ответ на вопрос от юзера
 def answer_user(value_game, value_task):
     result = False
     # получаем ответ юзера
@@ -60,11 +67,18 @@ def answer_user(value_game, value_task):
     # проверяем результат в зависимости от игры
     if value_game == 'even':
         result = checking_answer_even(value_task, answer_user)
-    elif value_game == 'calc':
-        result = checking_answer_calc(value_task, int(answer_user))
+    elif value_game == 'calc' or value_game == 'gcd':
+        result = checking_answer(value_task, int(answer_user))
     return result
 
 
+# вычисляем результат игры brain_calc
 def expression_calculation(random_number_1, random_number_2, random_sign):
-    res = eval(f'{random_number_1} {random_sign} {random_number_2}')
-    return res
+    result = eval(f'{random_number_1} {random_sign} {random_number_2}')
+    return result
+
+
+# вычисляем результат игры brain_gcd
+def calculation_result_gcd(random_number_1, random_number_2):
+    result = math.gcd(random_number_1, random_number_2)
+    return result
