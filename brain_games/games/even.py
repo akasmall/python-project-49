28f4478e46c17424bfe9965cf23beeed00d53c93
akasmall <aka_sm@ro.ru>
2023-10-26 13:59:36 +0300

    refactoring of all project modules, 1 module, release 0.2.005d

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,14 +6,14 @@ EVEN_UPPER_BOUND = 99
 
 
 def is_number_even(number):
-    return number % 2
+    return not number % 2
 
 
 def get_question_solution(
         lower_bound=EVEN_LOWER_BOUND, upper_bound=EVEN_UPPER_BOUND):
     number = random.randint(lower_bound, upper_bound)
     if is_number_even(number):
-        correct_answer = 'no'
-    else:
         correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
     return str(number), correct_answer
